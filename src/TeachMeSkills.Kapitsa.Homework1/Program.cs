c0786a3aeaa48aea148b6bc57f711a9499b27b05
KapitsaAnton <kapicaanton@gmail.com>
2021-09-26 20:02:29 +0300

    feat: add solution of homework 1

---
+++ src/TeachMeSkills.Kapitsa.Homework1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeachMeSkills.Kapitsa.Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Anton Kapitsa!");
+        }
+    }
+}
